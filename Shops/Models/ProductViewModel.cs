fdb927530b82d6028eea79c640b802052e6e0919
SimonShearn <simon@sgurr.co.uk>
2016-10-30 15:44:07 +0000

    Add files via upload

---
+++ Shops/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Shops.Models;
+
+namespace Shops.Models
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Shops.product> products;
+    }
+}
