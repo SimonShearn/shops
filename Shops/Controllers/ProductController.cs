16efe6268b3232c4594ea9ffe6536936870ac738
SimonShearn <simon@sgurr.co.uk>
2016-10-30 15:42:55 +0000

    Add files via upload

---
+++ Shops/Controllers/ProductController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shops.Models;
+
+namespace Shops.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ShopsEntities db = new ShopsEntities();
+        private const string ALL = "All";
+
+        //
+        // GET: /Product/
+        public ActionResult Index(string shopFilter, int? maxPrice)
+        {
+            List<String> list = (from s in db.shops select s.name).Distinct().ToList();
+            list.Insert(0, ALL);
+            
+            ViewBag.ShopList = list;
+            ViewBag.ShopFilter = shopFilter;
+            var products = db.products.Include(p => p.shop);
+            ProductViewModel VM = new ProductViewModel();
+
+            products = products.OrderByDescending(p => p.price);
+            if ((shopFilter != null) && (shopFilter != ALL))
+            {
+                products = products.Where(p => p.shop.name == shopFilter);
+            }
+            if ((maxPrice != null))
+            {
+                products = products.Where(p => p.price <= maxPrice);
+            }
+
+            VM.products = products.ToList();
+            return View(VM);
+        }
+
+
+
+        //TODO
+        public ActionResult GetProducts(string shopFilter, int? maxPrice)
+        {
+            JsonResult result;
+            try
+            {
+                var products = db.products.Include(p => p.shop);
+
+                if ((shopFilter != null) && (shopFilter != ALL))
+                {
+                    products = products.Where(p => p.shop.name == shopFilter);
+                }
+                if ((maxPrice != null))
+                {
+                    products = products.Where(p => p.price <= maxPrice);
+                }
+
+                result = Json(products.ToList(), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                String errorMessage = ex.Message;
+                if (ex.InnerException != null) errorMessage = ex.InnerException.Message.Substring(0, Math.Max(ex.InnerException.Message.Length, 20));
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+            return result;
+        }
+
+
+        //
+        // GET: /Product/Details/5
+        public ActionResult Details(int id = 0)
+        {
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        //
+        // GET: /Product/Create
+        [Authorize(Users = "administrator")]
+        public ActionResult Create()
+        {
+            ViewBag.shop_id = new SelectList(db.shops, "id", "name");
+            return View();
+        }
+
+
+        //
+        // POST: /Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Users = "administrator")]
+        public ActionResult Create(product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.shop_id = new SelectList(db.shops, "id", "name", product.shop_id);
+            return View(product);
+        }
+
+
+        //
+        // GET: /Product/Edit/5
+
+        [Authorize(Users = "administrator")]
+        public ActionResult Edit(int id = 0)
+        {
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.shop_id = new SelectList(db.shops, "id", "name", product.shop_id);
+            return View(product);
+        }
+
+
+        //
+        // POST: /Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Users = "administrator")]
+        public ActionResult Edit(product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.shop_id = new SelectList(db.shops, "id", "name", product.shop_id);
+            return View(product);
+        }
+
+
+        //
+        // GET: /Product/Delete/5
+        public ActionResult Delete(int id = 0)
+        {
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        //
+        // POST: /Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Shops/Controllers/ShopController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shops.Controllers
+{
+    [Authorize(Users = "administrator")]
+    public class ShopController : Controller
+    {
+        private ShopsEntities db = new ShopsEntities();
+
+        //
+        // GET: /Shop/
+        public ActionResult Index()
+        {
+            return View(db.shops.ToList());
+        }
+
+        //
+        // GET: /Shop/Details/5
+        public ActionResult Details(int id = 0)
+        {
+            shop shop = db.shops.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        //
+        // GET: /Shop/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Shop/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Users = "administrator")]
+        public ActionResult Create(shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.shops.Add(shop);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shop);
+        }
+
+        //
+        // GET: /Shop/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            shop shop = db.shops.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        //
+        // POST: /Shop/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shop).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shop);
+        }
+
+        //
+        // GET: /Shop/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            shop shop = db.shops.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        //
+        // POST: /Shop/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            shop shop = db.shops.Find(id);
+            db.shops.Remove(shop);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
